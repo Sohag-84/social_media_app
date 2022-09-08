1cb9aa63aabed651caf7e6af347e25d30f61ba93
IH Sohag <injamulhaqsohag19977@gmail.com>
2022-09-08 14:18:34 +0600

    modelling user data

--- lib/resources/auth_method.dart
+++ lib/resources/auth_method.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:social_media_app/model/user.dart';
 import 'package:social_media_app/resources/storage_method.dart';
 
 class AuthMethod {
@@ -29,55 +30,56 @@ class AuthMethod {
             .uploadImageToStorage('profile-picture', file, false);
 
         //add user to our database
-        await _firestore.collection('users').doc(_credential.user!.uid).set({
-          'username': username,
-          'uid': _credential.user!.uid,
-          'email': email,
-          'bio': bio,
-          'followers': [],
-          'following': [],
-          'photoUrl': photoUrl,
-        });
+        UserModel userModel = UserModel(
+          email: email,
+          uid: _credential.user!.uid,
+          photoUrl: photoUrl,
+          username: username,
+          bio: bio,
+          followers: [],
+          following: [],
+        );
+
+        await _firestore.collection('users').doc(_credential.user!.uid).set(userModel.toJson());
         result = 'registration successful';
       }
-    } on FirebaseAuthException catch(e){
-      if(e.code == 'invalid-email'){
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'invalid-email') {
         result = 'Write a valid email';
-      }
-      else if (e.code == 'weak-password') {
+      } else if (e.code == 'weak-password') {
         result = 'The password should be at least 6 character';
       } else if (e.code == 'email-already-in-use') {
         result = 'The account already exists for that email.';
       }
-    }
-    catch (error) {
+    } catch (error) {
       result = error.toString();
     }
     return result;
   }
 
-  Future<String> signInUser({required email,
-    required password,}) async{
+  Future<String> signInUser({
+    required email,
+    required password,
+  }) async {
     String result = 'some error occured';
-    try{
-      if(email.isNotEmpty ||
-          password.isNotEmpty){
-        final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-            email: email,
-            password: password,
+    try {
+      if (email.isNotEmpty || password.isNotEmpty) {
+        final credential =
+            await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: password,
         );
         result = 'Successfully login';
-      }else{
+      } else {
         result = 'Please enter all the field';
       }
-    }on FirebaseAuthException catch (e) {
+    } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
         result = 'No user found for that email.';
       } else if (e.code == 'wrong-password') {
         result = 'Wrong password provided for that user.';
       }
-    }
-    catch (e){
+    } catch (e) {
       result = e.toString();
     }
     return result;
